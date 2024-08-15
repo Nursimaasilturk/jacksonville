92c52b5f99bb32604dd4432e2aceefee4089e99b
Nursima Asilturk <nursima.asilturk99@gmail.com>
2024-08-15 16:18:05 +0300

    hover is done

--- shared/js/scripts.js
+++ shared/js/scripts.js
@@ -1,4 +1,13 @@
 $(function(){
-	$('#header').load('shared/header.html');
-	$('#footer').load('shared/footer.html');	
+	$('#header').load('shared/header.html',function(){
+		initializeEventListeners();
+	});
+	$('#footer').load('shared/footer.html');
+	function initializeEventListeners(){
+		$('.categories .categories__item').click(function(){
+			$('.categories .categories__item').find('a').removeClass('active');
+			var child = $(this).find('a');
+			child.addClass('active');	
+		});
+	}
 });
